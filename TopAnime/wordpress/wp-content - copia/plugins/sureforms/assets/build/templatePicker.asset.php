65095d9e7b167996832544c7ac82b5b7485a0d24
SugamP22 <Sugampoudel528@gmail.com>
2025-04-10 11:45:11 +0200

    añadiendo un carpeta de wordpree

---
+++ TopAnime/wordpress/wp-content - copia/plugins/sureforms/assets/build/templatePicker.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-api-fetch', 'wp-components', 'wp-element', 'wp-hooks', 'wp-i18n'), 'version' => '20b486c3f8049cf0e202');
---
+++ TopAnime/wordpress/wp-content/uploads/uag-plugin/assets/1000/uag-js-1326.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-6f0cd200' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-da6a4fc4' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-26a2717b' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-a09791e7' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-96399023' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-fc45789f' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-cab9d2e8' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-f6912abe' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-61abdc0a' );
+});
+window.addEventListener( 'load', function() {
+	UAGBButtonChild.init( '.uagb-block-8ab817e9' );
+});
+ });
